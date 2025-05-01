8541b76873fb7ead275a15b033368fade3492c32
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-05-01 21:52:13 +0300

    working with events

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 function App() {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input
+        type="checkbox"
+        id="pepperoni"
+        checked={false}
+        aria-checked={false}
+      />
+      <label htmlFor="pepperoni">Add pepperoni</label>
+
+       <h2>Your Toppings:</h2>
+        <ul>
+          <li>Cheese</li>
+        </ul>
+        </div>
+      );
+    }
 
 export default App;
